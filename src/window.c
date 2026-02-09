7744815ba234cf5d8871f47c9e9d0dcab18e7e70
Raazkatwal <raazkatwal5@gmail.com>
2026-02-09 09:34:44 +0545

    feat(UI): Add simple UI

--- src/window.c
+++ src/window.c
@@ -1,11 +1,23 @@
 #include "window.h"
+#include <gtk/gtk.h>
 
 GtkWindow *servctl_window_new(GtkApplication *app) {
+  GtkBuilder *builder;
   GtkWidget *window;
 
-  window = gtk_application_window_new(app);
-  gtk_window_set_title(GTK_WINDOW(window), "ServCtl");
-  gtk_window_set_default_size(GTK_WINDOW(window), 400, 300);
+  builder = gtk_builder_new_from_file("resources/ui/main.ui");
+
+  window = GTK_WIDGET(gtk_builder_get_object(builder, "main_window"));
+
+  gtk_window_set_application(GTK_WINDOW(window), app);
+
+  GtkCssProvider *css_provider = gtk_css_provider_new();
+  gtk_css_provider_load_from_path(css_provider, "resources/style.css");
+  gtk_style_context_add_provider_for_display(gdk_display_get_default(),
+                                             GTK_STYLE_PROVIDER(css_provider),
+                                             GTK_STYLE_PROVIDER_PRIORITY_USER);
+
+  g_object_unref(builder);
 
   return GTK_WINDOW(window);
 }
